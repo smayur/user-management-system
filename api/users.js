e8208c2076b009cf78ef4ea809c78830c9a3b7e1
mayurS <mayurs@prdxn.com>
2021-01-19 18:12:16 +0530

    login functionality is added

--- api/users.js
+++ api/users.js
@@ -3,9 +3,16 @@ const router = express.Router();
 let usersData = require('../usersData.json');
 
 
-router.get('/', (req, res) =>{
-  res.json(usersData);
+// Login
+router.get('/login', (req, res, next) =>{
+  let isThere = false;
+  for (let i = 0; i < usersData.length; i++) {
+    if ( req.body.username === usersData[i].username && req.body.password === usersData[i].password ) {
+      isThere = true
+      return res.send(`Hi ${req.body.username}, Welcome to dashboard `);
+    } 
+  }
+  !isThere ? res.send(`Please check login credentials!`) : next()
 });
-
 
 module.exports = router
