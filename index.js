ef27822b00dfde66b26b8405d6cba04c61f7d770
mayurS <mayurs@prdxn.com>
2021-01-19 15:19:27 +0530

    create userData.json file

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const express = require(express);
+const express = require('express');
+const router = express.Router()
 
 const app = express();
+
+app.get('/', (req, res) =>{
+  console.log('Hi mayur, success is ahead!!!');
+});
+
+app.listen('5000', () => {
+  console.log('Listening on Port 5000');
+});
