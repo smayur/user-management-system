666b960eab21277af4c648e0d7d8b326e6dac575
mayurS <mayurs@prdxn.com>
2021-01-19 21:51:03 +0530

    about functionality for user is added

--- api/users.js
+++ api/users.js
@@ -1,9 +1,9 @@
+const { json } = require('express');
 const express = require('express');
 const session = require("express-session");
 const fs = require("fs");
 const router = express.Router();
 let usersData = require('../usersData.json');
-
 
 router.use(
   session({
@@ -13,6 +13,7 @@ router.use(
   })
 );
 
+
 // Login
 router.post('/login', (req, res, next) =>{
   let isThere = false;
@@ -20,15 +21,38 @@ router.post('/login', (req, res, next) =>{
     if ( req.body.username === usersData[i].username && req.body.password === usersData[i].password ) {
       isThere = true
       req.session.username = req.body.username;
-      return res.send(`Hi ${req.body.username}, Welcome to dashboard `);
+      return res.send(`Hi ${req.body.username}, Welcome to dashboard, Now tell us something about you..`);
     } 
   }
   !isThere ? res.send(`Please check login credentials!`) : next()
 });
 
 
+// Add data about user
+router.post('/about', (req, res, next) =>{
+  if ( req.session.username != undefined ) {
+    for (let i = 0; i < usersData.length; i++) {
+      if ( req.session.username === usersData[i].username ) {
+        let reqObject = req.body,
+            user = usersData[i],
+            aboutUser = user['about'] = {};
+        for (var key of Object.keys(reqObject)) {
+          aboutUser[key] = reqObject[key];
+        }
+        fs.writeFile('usersData.json', JSON.stringify(usersData), err => {
+          if (err) console.log(err);
+        })
+        return res.send(`Thanks ${req.session.username}, Now we can know you better..`);
+      } 
+    }
+  } else res.send("session expired, Please login again");
+  next();
+});
+
+
 // Logout user
 router.post('/logout', (req, res, next) =>{
+  console.log(`${req.session.username} log out`);
   req.session.destroy();
   next();
 });
